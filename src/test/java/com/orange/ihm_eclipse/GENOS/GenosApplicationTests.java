287dcb2ea60ecba0655bb0426ebdfbc2b310e331
MarvinP3 <49071645+MarvinP3@users.noreply.github.com>
2019-12-16 16:02:49 -0400

    J'ai essayer de tester le microservice Hello World avec un test unitaire dans TestWebApp mais il a pas l'air de le prendre en compte

--- src/test/java/com/orange/ihm_eclipse/GENOS/GenosApplicationTests.java
+++ src/test/java/com/orange/ihm_eclipse/GENOS/GenosApplicationTests.java
@@ -1,7 +1,13 @@
 package com.orange.ihm_eclipse.GENOS;
+import com.orange.ihm_eclipse.GENOS.web.controller.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 class GenosApplicationTests {
@@ -9,5 +15,14 @@ class GenosApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+    
+	@Test
+	void testHelloWorld() {
+		
+		
+		HelloWorld helloWorld;		
+		assertEquals("Hello World", "Hello World" );
 
+	}
 }
+
